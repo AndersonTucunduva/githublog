b3a9b7fa65c93703051d3c995e44b5833c85512b
Anderson Tucunduva <andersonpreuhs@gmail.com>
2023-11-10 11:01:40 -0300

    third

--- src/app/components/Summary.tsx
+++ src/app/components/Summary.tsx
@@ -1,20 +1,18 @@
 export default function Summary() {
   return (
-    <>
-      <div className="max-h-64 max-w-full rounded bg-basepost p-8">
-        <div className="mb-5 flex justify-between">
-          <h1 className="text-xl text-basetitle">
-            Primeiro congresso programação
-          </h1>
-          <h2 className="ml-2 text-sm text-basespan">há 1 dia</h2>
-        </div>
-        <span className="text-base text-basetext">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
-          voluptatum sit cumque enim iusto expedita iure laboriosam assumenda
-          doloribus corrupti libero eos, cum numquam ducimus nesciunt amet,
-          dolorum ab similique.
-        </span>
+    <div className="max-h-64 max-w-full rounded bg-basepost p-8">
+      <div className="mb-5 flex justify-between">
+        <h1 className="text-xl text-basetitle">
+          Primeiro congresso programação
+        </h1>
+        <h2 className="ml-2 text-sm text-basespan">há 1 dia</h2>
       </div>
-    </>
+      <span className="text-base text-basetext">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+        voluptatum sit cumque enim iusto expedita iure laboriosam assumenda
+        doloribus corrupti libero eos, cum numquam ducimus nesciunt amet,
+        dolorum ab similique.
+      </span>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,21 @@ import Summary from './components/Summary'
 
 export default function Home() {
   return (
-    <>
-      <div className="h-screen bg-basebackground">
-        <div className="m-auto max-w-6xl">
-          <Header />
-        </div>
-        <div className="m-auto -mt-9 max-w-4xl bg-baseprofile">
-          <Slogan />
-        </div>
-        <div className="m-auto max-w-4xl">
-          <Search />
-        </div>
-        <div className="m-auto mt-12 grid max-w-4xl grid-cols-2 gap-8">
-          <Summary />
-          <Summary />
-          <Summary />
-          <Summary />
-          <Summary />
-        </div>
+    <div className="  bg-basebackground">
+      <div className="z-10 m-auto max-w-6xl">
+        <Header />
       </div>
-    </>
+      <div className="z-20 m-auto -mt-9 max-w-4xl bg-baseprofile">
+        <Slogan />
+      </div>
+      <div className="m-auto max-w-4xl">
+        <Search />
+      </div>
+      <div className="m-auto mt-12 grid max-w-4xl grid-cols-2 gap-8">
+        <Summary />
+        <Summary />
+        <Summary />
+      </div>
+    </div>
   )
 }
